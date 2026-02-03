120a189384a6a05c0d4160b1a116ce63e1c72547
Jose Ivan Lanestosa Pelayo <ivanlanestosa8@gmail.com>
2026-02-03 12:52:05 -0600

    se agregó el index del proyecto we

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>
+        BIEN VENIDOS a la página del equipo Minecraft
+    </h1>
+    <h2>
+        Integrantes
+    </h2>
+    <ul>
+        <li><a href="./ivan.docx">Lanestosa Pelayo Jose Ivan</a></li>
+        <li><a href="./cesar.docx">Cesar Gandhi Monterrosa Gomez</a></li>
+        <li><a href="./margarita.docx">Margarita Ramirez Perez</a></li>
+        <li><a href="./gaspar.docx">Cristian gaspar Perez Aguilar</a></li>
+    </ul>
+</body>
+</html>
